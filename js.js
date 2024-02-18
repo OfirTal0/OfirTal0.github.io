51a5ec929538fc8f4a899ac7a759a9973435edd6
Ofir Tal <140008644+OfirTal0@users.noreply.github.com>
2024-02-18 21:29:06 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -8,10 +8,19 @@ const flightDate = new Date('2024-05-22');
 
 const timeDifference = flightDate.getTime() - currentDate.getTime();
 const daysLeft = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+const imgArray = ["greece.png","vacations.png","kebab.png"];
 
 function update() {
     const squaresToUpdate = [];
-    localStorage.setItem("update",JSON.stringify({ currentDay }))
+    const storedUpdatedDay = localStorage.getItem("UpdatedDay");
+
+    if (!storedUpdatedDay) {
+        localStorage.setItem("UpdatedDay", currentDay);
+    }
+
+    if (storedUpdatedDay == currentDay ) {
+        alert("No more days to update. Wait patiently for tomorrow")
+    }
     for (let i=1; i<=currentDay; i++) {
         let squre = document.getElementById(`squre${i}`);
         if (squre) {
@@ -20,21 +29,32 @@ function update() {
     }
 
     function updateSquareWithDelay(index) {
-            if (index < squaresToUpdate.length) {
-                const square = squaresToUpdate[index];
-                square.innerHTML = "X";
-                square.className = "squreX";
 
-                // Set a delay of 500 milliseconds (adjust as needed)
-                setTimeout(() => {
-                    updateSquareWithDelay(index + 1);
-                }, 200);
-            }
+        if (index < squaresToUpdate.length) {
+            const square = squaresToUpdate[index];
+
+            square.innerHTML = "";
+    
+            const imgElement = document.createElement("img");
+    
+            const randomIndex = Math.floor(Math.random() * imgArray.length);
+            imgElement.src = imgArray[randomIndex];
+            imgElement.className = "imglogo"
+            square.appendChild(imgElement);
+            square.className = "squreX";
+    
+            setTimeout(() => {
+                updateSquareWithDelay(index + 1);
+            }, 300);
         }
-    updateSquareWithDelay(0);
-
-} 
-
+    }
+    
+    if (storedUpdatedDay) {
+        updateSquareWithDelay(parseInt(storedUpdatedDay, 10));
+    } else {
+        updateSquareWithDelay(0);
+    }
+}
 function Info() {
     return (
         <>
@@ -42,7 +62,7 @@ function Info() {
             <h1>Desperation table</h1>
             <h2> You have {daysLeft} days left until your trip </h2>
             <h3> We are now at {currentMonth} month</h3>
-            <button onClick={update}>Update days left</button>
+            <button className="button" onClick={update}>Update days left</button>
         </div>
         </>
     ); 
@@ -50,18 +70,23 @@ function Info() {
 
 function CreateTable() {
     let squres = [] 
-    var updateDate= localStorage.getItem("update")
-    if (updateDate) {
-        updateDate = JSON.parse(updateDate);
+    const storedUpdatedDay = localStorage.getItem("UpdatedDay");
+    parseInt(storedUpdatedDay, 10)
+    if (storedUpdatedDay) {
         for (let i=1; i <= lastDayOfMonth; i++) {
-            if (i<=updateDate.currentDay) {
-                squres.push(<div className="squreX" key={i} id={`squre${i}`}>X</div>)  
+            if (i<=parseInt(storedUpdatedDay, 10)) {
+                const randomIndex = Math.floor(Math.random() * imgArray.length);
+                squres.push(<div className="squreX" key={i} id={`squre${i}`}>
+                <img className="imglogo" src={imgArray[randomIndex]} />
+                </div>)  
             } else {
-                squres.push(<div className="squre" key={i} id={`squre${i}`}>{i}</div>)  
+                squres.push(<div className="squre" key={i} id={`squre${i}`}>
+                {i}
+                </div>)  
             }
         }
     } else {
-        for (let i = 1; i <= lastDayOfMonth; i++) {
+                for (let i = 1; i <= lastDayOfMonth; i++) {
             squres.push(<div className="squre" key={i} id={`squre${i}`}>{i}</div>);
         }
     }
@@ -72,9 +97,10 @@ function CreateTable() {
             </div>
         </>
     );
+    update()  
 
-update()  
 }
+
 
 function Main() {
     return(
